cfc27ae8a0215ada152ac8d5d8a005a706536757
Gatasso <galassoworkstation@gmail.com>
2024-07-15 18:11:58 -0300

    First Commit

---
+++ src/test/java/me/dio/API/Banco/ApiBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.API.Banco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
