f13a87535c7f7ccba610791585ff80b3c0cc2ad3
Gatasso <galassoworkstation@gmail.com>
2024-07-16 19:17:15 -0300

    :wrench: fix: Corrige erro de CORS, para nova tentativa de Deploy Tentativa: 2

--- src/main/java/me/dio/API/Banco/ApiBancoApplication.java
+++ src/main/java/me/dio/API/Banco/ApiBancoApplication.java
@@ -1,11 +1,13 @@
 package me.dio.API.Banco;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class ApiBancoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiBancoApplication.class, args);
 	}
